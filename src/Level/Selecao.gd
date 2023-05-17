93dec0608ead836197e5c1992b30cb498dd5cf21
ninodemeterko <ninodemeterko@hotmail.com>
2023-05-17 20:40:28 -0300

    fix carregar nivel

--- src/Level/Selecao.gd
+++ src/Level/Selecao.gd
@@ -1,24 +1,16 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 onready var pato = ResourceLoader.load("res://src/Actors/Player.tscn").instance()
+var carregandoNivel = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		$Fundo1/Fase1.select_nivel()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	hide()
+
 #func _process(delta):
 #	pass
-
-
-func _on_FaseBotao_pressed():
-	hide()
-	pass
--- src/UserInterface/NivelSelecao.gd
+++ src/UserInterface/NivelSelecao.gd
@@ -8,33 +8,36 @@ export(String, FILE, "*.tscn") var nivel
 
 var disponivel = false
 var completado = false
-var selecionado = false
+export var selecionado = false setget selecionar
 
 onready var _textura = $Botao
 
 onready var selecaoNivel = get_parent().get_parent()
 
-func selecionar():
-	selecionado = true
+func selecionar(value):
+	selecionado = value
 	# _textura.texture.
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func select_nivel():
-	var root = get_tree().get_root().get_child(0)
-	var current_scene = root.get_child(0)
+	if not selecaoNivel.carregandoNivel:
+		selecaoNivel.carregandoNivel = true
+		var root = get_tree().get_root().get_child(0)
+		var current_scene = root.get_child(0)
 
-	current_scene.queue_free()
-	# 2. Load the new scene.
-	var new_scene = ResourceLoader.load(nivel)
+		current_scene.queue_free()
+		# 2. Load the new scene.
+		var new_scene = ResourceLoader.load(nivel)
 
-	# 3. Instance the new scene.
-	current_scene = new_scene.instance()
-	root.add_child(current_scene)
-	current_scene.add_child(selecaoNivel.pato)
-	selecaoNivel.pato.position = Vector2(120, 530)
-	pass
+		# 3. Instance the new scene.
+		current_scene = new_scene.instance()
+		root.add_child(current_scene)
+		current_scene.add_child(selecaoNivel.pato)
+		selecaoNivel.pato.position = Vector2(120, 530)
+		selecaoNivel.carregandoNivel = false
+		pass
 
 
 func _on_FaseBotao_pressed():

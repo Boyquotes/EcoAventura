2ead4106c793b6804092c0aca4eeb46f763bb73d
ninodemeterko <ninodemeterko@hotmail.com>
2023-05-17 22:30:40 -0300

    Consertado dependencias quebradas do merge

--- src/Main/Game.gd
+++ src/Main/Game.gd
@@ -7,6 +7,8 @@ extends Node
 # that is to say, another node or script should not access them.
 onready var _pause_menu = $InterfaceLayer/PauseMenu
 
+onready var level = $Level
+
 func _init():
 	OS.min_window_size = OS.window_size
 	OS.max_window_size = OS.get_screen_size()
